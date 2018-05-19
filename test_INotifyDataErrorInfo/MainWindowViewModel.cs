c8bbad25a876a48a38b8418c303dbdab6b9d0c6f
TowaOnohara <jack.jilljp2@gmail.com>
2018-05-20 01:33:55 +0900

    SimpleMVVM処理を追加。

---
+++ test_INotifyDataErrorInfo/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_INotifyDataErrorInfo
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private List<Person> _People;
+        public List<Person> People
+        {
+            get { return _People; }
+            set
+            {
+                if (_People == value)
+                    return;
+                _People = value;
+                RaisePropertyChanged(nameof(People));
+
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            People = new List<Person>
+            {
+                new Person(){ Name = "Taro", Age = 10 },
+                new Person(){ Name = "Jiro", Age =  9 },
+            };
+        }
+    }
+
+    public class Person : BaseViewModel
+    {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Name == value)
+                    return;
+                _Name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+
+        private int _Age;
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                if (_Age == value)
+                    return;
+                _Age = value;
+                RaisePropertyChanged(nameof(Age));
+            }
+        }
+    }
+}
---
+++ test_INotifyDataErrorInfo/SimpleMVVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace test_INotifyDataErrorInfo
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion    
+    }
+
+    public class DelegeteCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : this._canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute?.Invoke(parameter);
+        }
+
+
+        public DelegeteCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this._execute = execute ?? throw new ArgumentException("execute");
+            this._canExecute = canExecute ?? throw new ArgumentException("canExecute");
+        }
+        public DelegeteCommand(Action<object> execute)
+        {
+            this._execute = execute ?? throw new ArgumentException("execute");
+        }
+    }
+}
